08a19eaf930d5ba94c0d2a958e7872ac46b422dd
Leejaebum <cjdrn0208@naver.com>
2020-11-29 22:50:36 +0900

    프로그래머스 / 연습문제 / Level1

---
+++ src/연습문제/문자열다루기기본/Solution.java
@@ -0,0 +1,26 @@
+package 연습문제.문자열다루기기본;
+
+class Solution {
+    public boolean solution(String s) {
+        int a=s.length();
+        boolean answer=true;
+        int cnt=0;
+        if(a==4||a==6){
+            for(int i=0; i<s.length(); i++){
+                char c=s.charAt(i);
+                if(!Character.isDigit(c)){
+                    cnt++;
+                }
+            }
+            if(cnt!=0){
+                answer=false;
+            }
+
+        }
+        else{
+            answer=false;
+        }
+
+        return answer;
+    }
+}
