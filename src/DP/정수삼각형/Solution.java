695e588ff021aaa42e676b78e75c78c2fded474b
Leejaebum <cjdrn0208@naver.com>
2020-12-23 18:01:37 +0900

    프로그래머스 / DP / Level3

---
+++ src/DP/정수삼각형/Solution.java
@@ -0,0 +1,30 @@
+package DP.정수삼각형;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        for(int i=1; i<triangle.length; i++){
+            for(int j=0; j<triangle[i].length; j++){
+                if(j==0){
+                    triangle[i][j]+=triangle[i-1][j];
+                }
+                else if(i==j){
+                    triangle[i][j]+=triangle[i-1][j-1];
+                }
+                else{
+                    triangle[i][j]=Math.max(triangle[i][j]+triangle[i-1][j],triangle[i][j]+triangle[i-1][j-1]);
+                }
+
+            }
+        }
+        
+        int answer=0;
+        for(int i=triangle.length-1; i<triangle.length; i++){
+            answer=triangle[i][0];
+            for(int j=0; j<triangle[i].length; j++){
+                answer=Math.max(answer,triangle[i][j]);
+            }
+
+        }
+        return answer;
+    }
+}
