c3c6f7bd363b2a91c33c1c65e72a3889bef1a892
Leejaebum <cjdrn0208@naver.com>
2021-01-12 18:13:10 +0900

    프로그래머스 / DFS / level3

---
+++ src/DFSandBFS/여행경로/Solution.java
@@ -0,0 +1,53 @@
+package DFSandBFS.여행경로;
+
+//testcase 통과 75점
+//test1번 오류
+//문제가 무엇인지 잘모르겠음
+
+import java.util.Arrays;
+import java.util.Comparator;
+class Solution {
+    static String[] answer={};
+    static boolean []visited={};
+    public String[] solution(String[][] tickets) {
+        Arrays.sort(tickets, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if(o1[0].equals(o2[0])) {
+                    return o1[1].compareTo(o2[1]);
+                }
+                else
+                    return o1[0].compareTo(o2[0]);
+
+            }
+        });
+
+
+        visited=new boolean[tickets.length];
+        answer=new String[tickets.length+1];
+        answer[0] = "ICN";
+        dfs(0, tickets, answer);
+
+        //System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+
+    public static boolean dfs(int depth, String[][] tickets, String[] answer) {
+        if(depth==tickets.length) {
+            return true;
+        }
+
+        for(int i=0; i<tickets.length; i++) {
+            if(visited[i] || !answer[depth].equals(tickets[i][0]))
+                continue;
+
+            visited[i] = true;
+            answer[depth+1] = tickets[i][1];
+            if(dfs(depth+1,tickets, answer))
+                return true;
+            visited[i] = false;
+        }
+
+        return false;
+    }
+}
