76f95d092c32687e1db4a75cae609efafd53b16c
Leejaebum <cjdrn0208@naver.com>
2021-02-03 17:52:15 +0900

    백준 / DP / 문제2156

--- src/DP/포도주시식/Retry.java
+++ src/DP/포도주시식/Retry.java
@@ -20,7 +20,7 @@ public class Retry {
         dp[2]=arr[1]+arr[2];
 
         //3잔연속 마실수 없는 것을 조건으로 생각해야한다.
-        
+
         for(int i=3; i<=N; i++){
             dp[i]=Math.max(dp[i-1],Math.max(dp[i-2]+arr[i],dp[i-3]+arr[i-1]+arr[i]));
         }
