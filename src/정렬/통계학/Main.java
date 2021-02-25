05235a7a01d1128456f0f17360362394e67f4d95
Leejaebum <cjdrn0208@naver.com>
2021-02-25 22:45:32 +0900

    백준 / 정렬 / 문제2108

--- src/정렬/통계학/Main.java
+++ src/정렬/통계학/Main.java
@@ -13,6 +13,7 @@ public class Main {
         int [] check =new int[8001];
         int max=0;
         int sum=0;
+        System.out.println(arr.length);
         for(int i=0;i<n;i++) {
             arr[i]=Integer.parseInt(br.readLine());
             sum+=arr[i];
