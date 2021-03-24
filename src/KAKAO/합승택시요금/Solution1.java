7442ffecf81120d8ad30d762efa4ed12db8bed01
Leejaebum <cjdrn0208@naver.com>
2021-03-24 16:19:39 +0900

    프로그래머스 / KAKAO / Level3

---
+++ src/KAKAO/합승택시요금/Solution1.java
@@ -0,0 +1,46 @@
+package KAKAO.합승택시요금;
+
+//플로이드 와샬 알고리즘을 통해 시간오류 해결
+//촤단경로 구하기 문제 해결법
+//1.다익스트라 알고리즘
+//2.플로이드 와샬 알고리즘
+import java.util.*;
+class Solution1{
+    static int dp[][];
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+
+
+        dp=new int[n+1][n+1];
+        for(int []t:dp){
+            Arrays.fill(t,100000000);
+        }
+
+        for(int i=0; i<fares.length; i++){
+            int x=fares[i][0];
+            int y=fares[i][1];
+            int z=fares[i][2];
+            dp[x][y]=z;
+            dp[y][x]=z;
+        }
+
+        for(int k=1; k<=n; k++){
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=n; j++){
+                    if(i==j){
+                        dp[i][j]=0;
+                    }
+                    else{
+                        dp[i][j]=Math.min(dp[i][j],dp[i][k]+dp[k][j]);
+                    }
+                }
+            }
+        }
+
+        int answer=100000000;
+        for(int i=1; i<=n; i++){
+            answer=Math.min(answer,dp[s][i]+dp[i][a]+dp[i][b]);
+        }
+        return answer;
+    }
+
+}
