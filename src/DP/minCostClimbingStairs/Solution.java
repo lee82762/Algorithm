3c9e68c1f18e75e0082aa7287887d286ffbb92c0
Leejaebum <cjdrn0208@naver.com>
2021-07-21 14:17:40 +0900

    리틀코드/ minCostClimbingStairs

---
+++ src/DP/minCostClimbingStairs/Solution.java
@@ -0,0 +1,20 @@
+package DP.minCostClimbingStairs;
+
+public class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int dp[]=new int[cost.length+1];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2; i<cost.length; i++){
+            dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];
+        }
+        int answer=0;
+        answer=Math.min(dp[cost.length-1],dp[cost.length-2]);
+        return answer;
+
+    }
+}
+
+
+
+
