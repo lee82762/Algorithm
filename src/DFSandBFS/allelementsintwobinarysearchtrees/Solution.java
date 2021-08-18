00ff72ee74fa8aed6c04d0dbc8945a60e59eee79
lee82762 <cjdrn0208@naver.com>
2021-08-18 22:34:13 +0900

    리틀코드 /All Elements in Two Binary Search Trees

---
+++ src/DFSandBFS/allelementsintwobinarysearchtrees/Solution.java
@@ -0,0 +1,47 @@
+package DFSandBFS.allelementsintwobinarysearchtrees;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+class Solution {
+    List<Integer> a1=new ArrayList<>();
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+
+        if(root1==null&&root2==null){
+            return a1;
+        }
+
+        dfs(root1);
+        dfs(root2);
+        Collections.sort(a1);
+
+
+        return a1;
+    }
+    private void dfs(TreeNode node){
+        if(node==null){
+            return;
+        }
+        else{
+            a1.add(node.val);
+            dfs(node.right);
+            dfs(node.left);
+        }
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
