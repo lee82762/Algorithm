aacb691f980a7183a047ec1515f3c26f1ac30c5b
lee82762 <cjdrn0208@naver.com>
2021-10-21 20:40:07 +0900

    프로그래머스 /월간코드챌린지3/ 1번

---
+++ src/월간코드챌린지/나머지가1되는수찾기/Solution.java
@@ -0,0 +1,15 @@
+package 월간코드챌린지.나머지가1되는수찾기;
+
+class Solution {
+    public int solution(int n) {
+        int answer=0;
+        for(int i=1; i<n; i++){
+            if(n%i==1){
+                answer=i;
+                break;
+            }
+
+        }
+        return answer;
+    }
+}
