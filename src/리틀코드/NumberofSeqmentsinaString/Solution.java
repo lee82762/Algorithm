b0d83ddd82db3fa26a7183c5665ab3e74930555e
lee82762 <cjdrn0208@naver.com>
2021-11-01 22:27:29 +0900

    리틀코드 / Number of Segments in a String

---
+++ src/리틀코드/NumberofSeqmentsinaString/Solution.java
@@ -0,0 +1,16 @@
+package 리틀코드.NumberofSeqmentsinaString;
+
+class Solution {
+    public int countSegments(String str) {
+        String[] arr = str.split("\\s+");
+
+        int count = 0;
+        for (String s : arr) {
+            if ((!s.equals("\\s+")) && s.length() > 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
