7e3ce29c8d3eb4bc5ff485bae1a26711970ddd57
lee82762 <cjdrn0208@naver.com>
2021-11-08 20:05:14 +0900

    백준 / DP / 문제9461번

---
+++ src/DP/파도반수열/Main.java
@@ -0,0 +1,23 @@
+package DP.파도반수열;
+
+import java.util.*;
+public class Main {
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++){
+            int a=sc.nextInt();
+            long dp[]=new long[101];
+            dp[1]=1;
+
+            dp[2]=1;
+
+            for(int j=3; j<=a; j++){
+                dp[j]=dp[j-2]+dp[j-3];
+            }
+
+            System.out.println(dp[a]);
+
+        }
+    }
+}
