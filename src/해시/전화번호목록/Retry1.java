a700678cf18b944d0ef771f99bf4a482b4a1ae06
lee82762 <cjdrn0208@naver.com>
2021-11-24 11:22:20 +0900

    프로그래머스/ 해시 / Level2

---
+++ src/해시/전화번호목록/Retry1.java
@@ -0,0 +1,23 @@
+package 해시.전화번호목록;
+
+import java.util.HashMap;
+class Retry1 {
+    public boolean solution(String[] phone_book) {
+
+        boolean answer=true;
+        HashMap<String,Integer>map=new HashMap<>();
+
+        for(int i=0; i<phone_book.length; i++){
+            map.put(phone_book[i],i);
+        }
+
+        for(int i=0; i<phone_book.length; i++){
+            for(int j=0; j<phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    answer=false;
+                }
+            }
+        }
+        return answer;
+    }
+}
