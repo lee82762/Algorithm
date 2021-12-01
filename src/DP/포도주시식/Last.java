b256212315c109d05a18be9f8bef2770d9e77170
lee82762 <cjdrn0208@naver.com>
2021-12-01 12:51:38 +0900

    백준/ DP / 문제2156

---
+++ src/DP/포도주시식/Last.java
@@ -0,0 +1,20 @@
+package DP.포도주시식;
+
+import java.util.Scanner;
+public class Last {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int arr[]=new int[10001];
+        for(int i=1; i<=N; i++){
+            arr[i]=sc.nextInt();
+        }
+        int dp[]=new int[10001];
+        dp[1]=arr[1];
+        dp[2]=arr[1]+arr[2];
+        for(int i=3; i<=N; i++){
+            dp[i]=Math.max(dp[i-1],Math.max(dp[i-2]+arr[i],dp[i-3]+arr[i-1]+arr[i]));
+        }
+        System.out.println(dp[N]);
+    }
+}
