d91b05ba6b4ca0acee18eb9b032d2ba0dccf6250
lee82762 <cjdrn0208@naver.com>
2022-01-04 13:36:08 +0900

    프로그래머스  / DFS  / Level3

---
+++ src/DFSandBFS/모두0으로만들기/Retry.java
@@ -0,0 +1,44 @@
+package DFSandBFS.모두0으로만들기;
+
+import java.util.*;
+class Retry {
+    private List<Integer>dp[];
+    long a2[];
+    long answer=0;
+    public long solution(int[] a, int[][] edges) {
+        a2=new long[a.length];
+        long sum=0;
+        for(int i=0; i<a.length; i++){
+            sum+=a[i];
+            a2[i]=a[i];
+        }
+        if(sum!=0){
+            return -1;
+        }
+        dp=new ArrayList[a.length];
+        for(int i=0; i<a.length; i++){
+            dp[i]=new ArrayList<>();
+        }
+        for(int i=0; i< edges.length; i++){
+            int x=edges[i][0];
+            int y=edges[i][1];
+            dp[x].add(y);
+            dp[y].add(x);
+
+        }
+
+        dfs(0,0);
+        return answer;
+    }
+
+    public void dfs(int x, int temp){
+
+        for(int a1 : dp[x]){
+            if(a1!=temp){
+                dfs(a1,x);
+            }
+        }
+        a2[temp]+=a2[x];
+        answer+= Math.abs(a2[x]);
+    }
+}
